25e73fdcebf8d3a1a6487c5d78d8e8e0675fcb4e
Duy Minh <duyminh081201@gmail.com>
2023-09-24 20:18:54 +0700

    update auto theme and animation on charging

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # 🔋 Battery Level Indicator Percentage
-## [Watch it on youtube](https://youtu.be/0gV3kmnLir0)
+
+## Project is forked from [bedimcode/battery-level-indicator](https://github.com/bedimcode/battery-level-indicator)
+
 ### 🔋 Battery Level Indicator Percentage
 
 - Battery Level Indicator Percentage Using HTML CSS & JavaScript .
+
 - Beautiful pill style battery.
+
+- Automatically updates the battery level.
+
+- Auto change theme follow system theme.
+
 - With animation of low battery, charging battery and full battery.
+
 - Color indicators according to the percentage of the battery.
+
 - Developed first with the Mobile First methodology, then for desktop.
+
 - Compatible with all mobile devices and with a beautiful and pleasant user interface.
 
-💙 Join the channel to see more videos like this. [Bedimcode](https://www.youtube.com/c/Bedimcode)
+### ⚠️ Project is not support edge browser. Please use chrome or firefox.
 
 ![preview img](/preview.png)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,132 @@
 /*=============== BATTERY ===============*/
-initBattery()
+const initBattery = () => {
+  const batteryLiquid = document.querySelector(".battery__liquid");
+  const batteryStatus = document.querySelector(".battery__status");
+  const batteryPercentage = document.querySelector(".battery__percentage");
+  const batteryCard = document.querySelector(".battery__card");
+  const batteryError = document.querySelector(".battery__error");
+  const iconCharging = document.querySelector(".icon-charging");
 
-function initBattery(){
-    const batteryLiquid = document.querySelector('.battery__liquid'),
-          batteryStatus = document.querySelector('.battery__status'),
-          batteryPercentage = document.querySelector('.battery__percentage')
-    
-    navigator.getBattery().then((batt) =>{
-        updateBattery = () =>{
-            /* 1. We update the number level of the battery */
-            let level = Math.floor(batt.level * 100)
-            batteryPercentage.innerHTML = level+ '%'
+  if (!navigator.getBattery) {
+    batteryCard.style.display = "none";
+    return;
+  } else {
+    batteryError.style.display = "none";
+  }
 
-            /* 2. We update the background level of the battery */
-            batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`
+  navigator.getBattery().then((batt) => {
+    updateBattery = () => {
+      /* 1. We update the number level of the battery */
+      let level = Math.floor(batt.level * 100);
+      batteryPercentage.innerHTML = level + "%";
 
-            /* 3. We validate full battery, low battery and if it is charging or not */
-            if(level == 100){ /* We validate if the battery is full */
-                batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`
-                batteryLiquid.style.height = '103%' /* To hide the ellipse */
-            }
-            else if(level <= 20 &! batt.charging){ /* We validate if the battery is low */
-                batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`
-            }
-            else if(batt.charging){ /* We validate if the battery is charging */
-                batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`
-            }
-            else{ /* If it's not loading, don't show anything. */
-                batteryStatus.innerHTML = ''
-            }
-            
-            /* 4. We change the colors of the battery and remove the other colors */
-            if(level <=20){
-                batteryLiquid.classList.add('gradient-color-red')
-                batteryLiquid.classList.remove('gradient-color-orange','gradient-color-yellow','gradient-color-green')
-            }
-            else if(level <= 40){
-                batteryLiquid.classList.add('gradient-color-orange')
-                batteryLiquid.classList.remove('gradient-color-red','gradient-color-yellow','gradient-color-green')
-            }
-            else if(level <= 80){
-                batteryLiquid.classList.add('gradient-color-yellow')
-                batteryLiquid.classList.remove('gradient-color-red','gradient-color-orange','gradient-color-green')
-            }
-            else{
-                batteryLiquid.classList.add('gradient-color-green')
-                batteryLiquid.classList.remove('gradient-color-red','gradient-color-orange','gradient-color-yellow')
-            }
-        }
-        updateBattery()
+      /* 2. We update the background level of the battery */
+      batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`;
+      
+      /* 3 We validate if the battery is charging */
+      if (batt.charging && level < 100) {
+        iconCharging.style.display = "block";
+        batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`;
+        batteryLiquid.classList.add("gradient-color-green");
+        batteryLiquid.classList.remove(
+          "gradient-color-red",
+          "gradient-color-orange",
+          "gradient-color-yellow"
+        );
+        batteryCard.style.setProperty(
+          "--status-battery-color",
+          "var(--gradient-color-green)"
+        );
+        batteryLiquid.style.animation = "animated-charging-battery 3s infinite";
+        return;
+      } else {
+        batteryLiquid.style.animation = "none";
+        iconCharging.style.display = "none";
+      }
 
-        /* 5. Battery status events */
-        batt.addEventListener('chargingchange', () => {updateBattery()})
-        batt.addEventListener('levelchange', () => {updateBattery()})
-    })
-}
+      /* 4. We validate full battery, low battery */
+      if (level == 100) {
+        /* We validate if the battery is full */
+        batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`;
+        batteryLiquid.style.height = "103%"; /* To hide the ellipse */
+      } else if ((level <= 20) & !batt.charging) {
+        /* We validate if the battery is low */
+        batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`;
+      } else {
+        /* If it's not loading, don't show anything. */
+        batteryStatus.innerHTML = "";
+      }
+
+      /* 5. We change the colors of the battery and remove the other colors */
+      if (level <= 20) {
+        batteryLiquid.classList.add("gradient-color-red");
+        batteryLiquid.classList.remove(
+          "gradient-color-orange",
+          "gradient-color-yellow",
+          "gradient-color-green"
+        );
+        batteryCard.style.setProperty(
+          "--status-battery-color",
+          "var(--gradient-color-red)"
+        );
+      } else if (level <= 40) {
+        batteryLiquid.classList.add("gradient-color-orange");
+        batteryLiquid.classList.remove(
+          "gradient-color-red",
+          "gradient-color-yellow",
+          "gradient-color-green"
+        );
+        batteryCard.style.setProperty(
+          "--status-battery-color",
+          "var(--gradient-color-orange)"
+        );
+      } else if (level <= 80) {
+        batteryLiquid.classList.add("gradient-color-yellow");
+        batteryLiquid.classList.remove(
+          "gradient-color-red",
+          "gradient-color-orange",
+          "gradient-color-green"
+        );
+        batteryCard.style.setProperty(
+          "--status-battery-color",
+          "var(--gradient-color-yellow)"
+        );
+      } else {
+        batteryLiquid.classList.add("gradient-color-green");
+        batteryLiquid.classList.remove(
+          "gradient-color-red",
+          "gradient-color-orange",
+          "gradient-color-yellow"
+        );
+        batteryCard.style.setProperty(
+          "--status-battery-color",
+          "var(--gradient-color-green)"
+        );
+      }
+    };
+    updateBattery();
+
+    /* 6. Battery status events */
+    batt.addEventListener("chargingchange", () => {
+      updateBattery();
+    });
+    batt.addEventListener("levelchange", () => {
+      updateBattery();
+    });
+  });
+};
+
+initBattery();
+
+/*=============== DARK / LIGHT MODE ===============*/
+const isLightTheme = window.matchMedia("(prefers-color-scheme: light)");
+
+isLightTheme.matches && document.body.classList.add("light-mode");
+
+isLightTheme.addEventListener("change", () => {
+  if (isLightTheme.matches) {
+    document.body.classList.add("light-mode");
+  } else {
+    document.body.classList.remove("light-mode");
+  }
+});
